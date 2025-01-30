a361236e92d5c9f1ba56ebb9191872f2f959cc16
Ali Maaz Akhter <150456791+alimaazakhter@users.noreply.github.com>
2025-01-30 12:35:34 +0530

    Add files via upload

---
+++ NUM.py
@@ -0,0 +1,32 @@
+#NUMpy in python
+
+import numpy as np
+
+arr = np.array([10, 20, 30, 40])
+
+print(arr)
+print(type(arr))
+
+#---------------------------------------------------------------------------------------------------------------
+
+#MultiDimensional Arrays(Metrices) in Python
+
+import numpy as np
+
+arr = np.array([[10, 20, 30, 40], [50, 60, 70, 80]])
+
+print(arr)
+
+#---------------------------------------------------------------------------------------------------------------
+
+# Normal Slicing in Numpy array>
+
+# import numpy as np
+
+# arr = np.array([10, 20, 30, 40])
+# print(arr[0:1])
+# print(arr[0:])
+# print(arr[3:])
+# print(arr[2:])
+# print(arr[:3])
+# print(arr[:2])
